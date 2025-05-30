f3d27e9181d1b839e9396cd16c29515a79bba3b0
ambrandt94 <ambrandt94@gmail.com>
2025-05-30 17:22:35 -0400

    element update

--- Runtime/UIElement.cs
+++ Runtime/UIElement.cs
@@ -4,16 +4,7 @@ namespace ChainLink.Core
 {
     public class UIElement : MonoBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        public static void TestDeal()
+        { }
     }
 }
